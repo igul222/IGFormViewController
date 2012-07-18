693c2b9ca3bfa954f4cb75fcd611f76b904ad9f4
Ishaan Gulrajani <igul222@gmail.com>
2012-07-18 16:18:02 -0400

    Add button control

---
+++ IGFormViewController/IGFormButton.h
@@ -0,0 +1,13 @@
+//
+//  IGFormButton.h
+//  Example
+//
+//  Created by Ishaan Gulrajani on 7/18/12.
+//  Copyright (c) 2012 Ishaan Gulrajani. All rights reserved.
+//
+
+#import "IGFormElement.h"
+
+@interface IGFormButton : IGFormElement
+
+@end
---
+++ IGFormViewController/IGFormButton.m
@@ -0,0 +1,13 @@
+//
+//  IGFormButton.m
+//  Example
+//
+//  Created by Ishaan Gulrajani on 7/18/12.
+//  Copyright (c) 2012 Ishaan Gulrajani. All rights reserved.
+//
+
+#import "IGFormButton.h"
+
+@implementation IGFormButton
+
+@end
