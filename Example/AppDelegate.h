d015ae4e66b73fb4d62fc337b07bf76b3d2f7820
Ishaan Gulrajani <igul222@gmail.com>
2012-07-18 15:08:09 -0400

    Initial Commit

---
+++ Example/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Ishaan Gulrajani on 7/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
